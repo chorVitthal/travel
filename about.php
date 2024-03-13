4b50b076e365ddb7bfc123bac823acd60f1b8a14
chorVitthal <147121794+chorVitthal@users.noreply.github.com>
2024-03-13 11:26:15 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>about</title>
+
+    <!-- swiper css link -->
+    <link rel="stylesheet"href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+
+    <!-- Font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+    <!-- Custom CSS File Link -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    
+    <!-- header section start -->
+    <section class="header">
+        <a href="home.php" class="logo">travel.</a>
+
+        <nav class="navbar">
+            <a href="home.php">home</a>
+            <a href="about.php">about</a>
+            <a href="package.php">package</a>
+            <a href="book.php">book</a>
+        </nav>
+        <div id="menu-btn" class="fas fa-bars"></div> 
+
+</section>
+    <!-- header section ends -->
+
+<div class="heading" style="background:url(images/header1.jpg)">
+    <h1>about us</h1>
+</div>
+ 
+<!-- about section starts -->
+
+<section class="about">
+    <div class="image">
+        <img src="images/about us.jpg" alt="">
+    </div>
+
+    <div class="content">
+        <h3>why choose us?</h3>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis officia nemo cumque, eos quis consequatur nulla,
+             nesciunt ea earum asperiores natus odit reprehenderit sint necessitatibus est dolorum consequuntur modi aliquam.</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima provident officia quae quia eligendi amet cum, 
+            assumenda neque atque unde velit expedita ab et excepturi maxime facere fuga similique fugiat.</p>
+        <div class="icons-container">
+            <div class="icons">
+                <i class="fas fa-map"></i>
+                <span>top destination</span>
+            </div>
+            <div class="icons">
+                <i class="fas fa-hand-holding-usd"></i>
+                <span>affordable price</span>
+            </div>
+            <div class="icons">
+                <i class="fas fa-headset"></i>
+                <span>24/7 guide services</span>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- about section ends -->
+
+<!-- reviews section start -->
+<section class="reviews">
+
+    <h1 class="heading-title">clients reviews</h1>
+
+    <div class="swiper reviews-slider">
+
+        <div class="swiper-wrapper">
+
+            <div class="swiper-slide slide">
+                <div class="starts">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>                
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci sint libero ab. Molestias pariatur
+                     ad ipsum possimus consectetur obcaecati laborum, reprehenderit dolores saepe officia quam doloremque nam magni corrupti quia!</p>
+                <span>traveler</span>
+                <h3>Sakshi Pawar</h3>
+                <img src="images/pic-1.jpg" alt="">
+            </div>
+
+            <div class="swiper-slide slide">
+                <div class="starts">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>                
+                </div>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis animi error numquam placeat perferendis ea cum aliquid impedit itaque 
+                    voluptates quo quaerat maxime, quidem, eius earum fuga! Eum inventore nemo nam perspiciatis ipsa tenetur expedita.</p>
+                <span>traveler</span>
+                <h3>Khushi Sharma</h3>
+                <img src="images/pic-2.jpg" alt="">
+            </div>
+
+            <div class="swiper-slide slide">
+                <div class="starts">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                     
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe eligendi quibusdam aspernatur possimus et ipsa, quod ullam alias ducimus omnis magni placeat sunt eos voluptatem minus obcaecati
+                     odio fugiat magnam molestias voluptatibus ad quae, qui aliquam illo! Reprehenderit, distinctio laborum.</p>
+                <span>traveler</span>
+                <h3>Poonam Pandey</h3>
+                <img src="images/pic-3.jpg" alt="">
+            </div>
+
+            <div class="swiper-slide slide">
+                <div class="starts">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>                
+                </div>
+                <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                    Corporis accusantium itaque quos, omnis iste nostrum.</p>
+                <span>traveler</span>
+                <h3>Prashant Modi</h3>
+                <img src="images/pic-4.jpg" alt="">
+            </div>
+
+            <div class="swiper-slide slide">
+                <div class="starts">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam iste delectus perferendis minima accusantium aperiam 
+                    ipsa, natus itaque, dolore ratione laborum quibusdam veritatis. Temporibus, tempora.</p>
+                <span>traveler</span>
+                <h3>Tukaram Mama</h3>
+                <img src="images/pic-5.jpg" alt="">
+            </div>
+
+            <div class="swiper-slide slide">
+                <div class="starts">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>                
+                    <i class="fas fa-star"></i>
+                </div>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente est 
+                    omnis tenetur in id sit dolor, qui laudantium at sint fuga et inventore repellendus sed libero vero quidem asperiores mollitia repudiandae! Quidem possimus dolore minima nemo laudantium quisquam aut in?</p>
+                <span>traveler</span>
+                <h3>Narendra Mirashe</h3>
+                <img src="images/pic-6.jpg" alt="">
+            </div>
+        </div>
+
+        <div class="swiper-pagination"></div>
+
+    </div>
+
+</section>
+
+
+
+<!-- reviews section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section start -->
+
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+            <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+            <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+            <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+        </div>
+
+        <div class="box">
+            <h3>extra links</h3>
+            <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+            <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+            <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+            <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <a href="#"> <i class="fas fa-phone"></i> +123-456-7890</a>
+            <a href="#"> <i class="fas fa-phone"></i> +111-222-3333</a>
+            <a href="#"> <i class="fas fa-envelope"></i> travelers@gmail.com</a>
+            <a href="#"> <i class="fas fa-map"></i> mumbai, india - 400104 </a>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+            <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+            <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+            <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+        </div>
+
+    </div>
+
+    <div class="credit"> created by <span>Mahesh & Vitthal</span> all rights reserved! </div>
+
+</section>
+
+
+            
+            
+            
+
+
+
+<!-- footer section ends -->
+
+<!-- swiper js link -->
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+<!-- Custom js file link-->
+<script src="javascript/script.js"></script>
+
+
+</body>
+</html>
---
+++ book_form.php
@@ -0,0 +1,30 @@
+<?php
+
+    $connection = mysqli_connect('localhost','root','','book_db');
+
+    if(isset($_POST['send'])){
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $address = $_POST['address'];
+        $location = $_POST['location'];
+        $guests = $_POST['guests'];
+        $arrivals = $_POST['arrivals'];
+        $leaving = $_POST['leaving']; 
+
+        $request = " insert into book_form(name, email,	phone, address,	location, guests, arrivals, leaving) values 
+        ('$name','$email','$phone','$address','$location','$guests','$arrivals','$leaving')";
+        
+
+         mysqli_query($connection, $request);
+
+         echo "<script> alert('Form successfully submited'); </script>";
+        
+        echo '<script>   window.open(
+            "book.php", "_blank");</script>';
+         //header('location:book.php');
+    }else{
+        echo 'something went wrong try again';
+    }
+
+?>
